27852d4236366111e96090e91d593dd14078f5d7
code043 <procoder043@gmail.com>
2026-05-19 21:52:45 -0300

    refactor: private dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,27 @@
-import { ReactNode } from "react";
+"use client"
+import { ReactNode, useEffect } from "react";
+import { useAuth } from "../context/auth-content";
+import { useRouter } from "next/navigation";
 
 export default function DashboardLayout({children}: { children: ReactNode}) {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.replace("/");
+    }
+  }, [user, loading, router]);
+
+  if (loading){
+  return (
+    <div className="m-30">
+      <p className="text-center">Loading...</p>
+    </div>
+    )
+  }
+
+  if (!user) return null;
   return (
     <div className=" py-20  bg-black text-white min-h-225">
       <h1 className="text-center text-4xl font-bold tracking-tight leading-tight">
