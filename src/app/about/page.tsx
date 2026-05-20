da1442e0189f3c4890bda10b773730c835ec8527
code043 <procoder043@gmail.com>
2026-05-20 17:03:56 -0300

    refactor: about page layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,19 +1,19 @@
 export default function AboutPage() {
   return (
     <section className="flex justify-center py-10">
-      <div className="p-20 w-148 h-90 hyphens-auto ">
-        <h1 className="text-4xl pb-8 text-center">Sobre o Projeto</h1>
+      <div className="p-20 w-200 min-h-100 hyphens-auto font-sans ">
+        <h1 className="text-4xl font-bold pb-8 text-center">Sobre o Projeto</h1>
 
-        <p >
+        <p className="text-center md:text-3xl">
           Este projeto reúne histórias de mistério, relatos da internet e
           narrativas inspiradas em eventos inexplicáveis.
         </p>
 
-        <p>
+        <p className="text-center md:text-3xl">
           O objetivo é explorar o desconhecido não necessariamente explicá-lo.
         </p>
 
-        <p>Todo conteúdo deve ser interpretado com senso crítico.</p>
+        <p className="text-center md:text-3xl">Todo conteúdo deve ser interpretado com senso crítico.</p>
       </div>
     </section>
   );
