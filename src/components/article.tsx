1b8812dab7c2af85697bcfcc6ee5f6c64eadb615
code043 <procoder043@gmail.com>
2026-05-21 00:44:59 -0300

    fix: post layout

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -19,19 +19,19 @@ export default function Article({ slug }: { slug: string }) {
   if (!post) return <p>Not found</p>;
   return (
     <div className="text-white bg-black px-10">
-      <div className="flex justify-center font-sans">
-        <div>
-          <h1 className=" text-4xl font-bold tracking-tight leading-tight mt-10 p-5">
+      <div className="flex flex-col items-center font-sans">
+        <div className="">
+          <h1 className="max-w-200 text-4xl font-bold tracking-tight leading-tight mt-30 mb-20">
             {post.title}
           </h1>
           {post.image && (
             <Image src={post?.image} alt="imagem" width={800} height={500} />
           )}
-          <h2 className="text-2xl my-3">{post.description}</h2>
+         <h2 className="text-2xl mt-10 px-3">{post.description}</h2>
           <div className="flex justify-center max-w-200">
-            <p className="p-1 mt-3 hyphens-auto">{post.content}</p>
+            <p className="px-3 mt-3 indent hyphens-auto">{post.content}</p>
           </div>
-          <p className="my-8">{setDate(post.createdAt)}</p>
+          <p className="my-8 px-3 mx-auto">{setDate(post.createdAt)}</p>
         </div>
       </div>
     </div>
